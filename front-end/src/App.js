58b38b4f9c7d447b949da5440e2ac4886e270cb2
Effolex <effolex@gmail.com>
2022-05-31 18:32:16 -0300

    feat(front):Created react router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <div>Login</div> } />
+          <Route path="/login" element={ <div>Login</div> } />
+          <Route path="/signin" element={ <div>Signin</div> } />
+          <Route path="/products" element={ <div>Products</div> } />
+          <Route
+            path="/loading"
+            element={
+              <div>
+                <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+                  Glass
+                </object>
+
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
