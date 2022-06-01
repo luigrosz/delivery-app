992762ee2163700717b09af6aa7e9875df23681e
Lui Grosz <luigrosz08@gmail.com>
2022-06-01 18:25:05 -0300

    fix: email already on database

--- back-end/src/service/signin.service.js
+++ back-end/src/service/signin.service.js
@@ -4,14 +4,12 @@ const { users } = require('../database/models');
 const signinUserToDB = async (email, password, name) => {
   try {
     const isEmailAlreadyInDb = await users.findOne({ where: { email } });
-    if (!isEmailAlreadyInDb === null) {
-      return { error: 'Email is already on Database' };
-    }
     const md5Password = md5(password);
     const created = await users.create({ name, email, password: md5Password, role: 'customer' });
     return created;
   } catch (e) { 
-    throw new Error(e);
+    console.log("entrou no email validate");
+    return { error: 'Email is already on Database' };
   }
 };
 
