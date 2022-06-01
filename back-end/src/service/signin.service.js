08e290110a3bccfbc05e7b4827d934c0e059082f
Lui Grosz <luigrosz08@gmail.com>
2022-06-01 18:30:47 -0300

    fix: lint

--- back-end/src/service/signin.service.js
+++ back-end/src/service/signin.service.js
@@ -3,12 +3,11 @@ const { users } = require('../database/models');
 
 const signinUserToDB = async (email, password, name) => {
   try {
-    const isEmailAlreadyInDb = await users.findOne({ where: { email } });
+    await users.findOne({ where: { email } });
     const md5Password = md5(password);
     const created = await users.create({ name, email, password: md5Password, role: 'customer' });
     return created;
   } catch (e) { 
-    console.log("entrou no email validate");
     return { error: 'Email is already on Database' };
   }
 };
