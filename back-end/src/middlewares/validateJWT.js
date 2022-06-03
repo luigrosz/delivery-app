dba13cf25e49b895a4cb4fc3e659b321ee58e017
Lui Grosz <luigrosz08@gmail.com>
2022-06-03 17:40:08 -0300

    fix(validateJWT): removing console.log

--- back-end/src/middlewares/validateJWT.js
+++ back-end/src/middlewares/validateJWT.js
@@ -6,9 +6,8 @@ const validateJWT = async (req, res, next) => {
     if (!token) return res.status(401).json({ error: 'No token provided.' });
 
     const key = fs.readFileSync(`${process.cwd()}/jwt.evaluation.key`, 'utf8');
-    jwt.verify(token, key, (err, decoded) => {
+    jwt.verify(token, key, (err, _decoded) => {
       if (err) return res.status(500).json({ error: 'Failed to authenticate token.' });
-      console.log(decoded);
       });
 
       next();
