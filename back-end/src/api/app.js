67afcc25cefdacf7fb9c4e3599192caf0fe41a86
Lui Grosz <luigrosz08@gmail.com>
2022-06-03 15:11:16 -0300

    feat: making the images folder public

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,6 +5,11 @@ const routes = require('../routes');
 const app = express();
 
 app.use(express.json());
+
+// torna a pasta de imagens publica
+const publicDirectory = path.join(__dirname, '../../public');
+app.use(express.static(publicDirectory));
+
 app.use(cors());
 app.use(routes);
 
