9b2690557614c103ec302120e6818969aaf76a14
Augusto Alves <85210880+GeZudo@users.noreply.github.com>
2022-06-04 02:31:15 -0300

    ⚗️ try fix tests

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const routes = require('../routes');
+
+const publicDirectory = path.join(__dirname, '../../public');
 
 const app = express();
 
+app.use(express.static(publicDirectory));
 app.use(express.json());
 app.use(cors());
 app.use(routes);
