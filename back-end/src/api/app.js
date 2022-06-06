fbe8e6afb87f7a80a62f8236946c6a58c8ee06fb
Augusto Alves <85210880+GeZudo@users.noreply.github.com>
2022-06-06 16:54:18 -0300

    🚚 Remove images to try fix tests

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const routes = require('../routes');
-
-const publicDirectory = path.join(__dirname, '../../public');
 
 const app = express();
 
-app.use(express.static(publicDirectory));
 app.use(express.json());
 app.use(cors());
 app.use(routes);
