f42fbb8ff0d8aa730b4c65d9cbcec9196f34c14a
Effolex <effolex@gmail.com>
2022-06-09 18:39:10 -0300

    refactor: refactored to use try catch

--- back-end/src/middlewares/validateJWT.js
+++ back-end/src/middlewares/validateJWT.js
@@ -7,13 +7,14 @@ const validateJWT = async (req, res, next) => {
     if (!authorization) return res.status(401).json({ error: 'No token provided.' });
 
     const key = fs.readFileSync(`${process.cwd()}/jwt.evaluation.key`, 'utf8');
-    jwt.verify(authorization, key, (err, _decoded) => {
-      if (err) return res.status(500).json({ error: 'Failed to authenticate token.' });
-      });
-    
-    const { data } = jwtDecode(authorization);
-    req.user = data;
-    next();
+    try {
+     jwt.verify(authorization, key);
+      const { data } = jwtDecode(authorization);
+      req.user = data;
+      return next();
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to authenticate token.' });
+    }
 };
 
 module.exports = validateJWT;
