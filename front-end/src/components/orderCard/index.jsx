a91a2d703d79fed5686631b0dac4f18c6378c634
Augusto Alves <85210880+GeZudo@users.noreply.github.com>
2022-06-09 02:48:49 -0300

    🩹 fix: change minor styles and change props names

--- front-end/src/components/orderCard/index.jsx
+++ front-end/src/components/orderCard/index.jsx
@@ -1,13 +1,16 @@
 import { Grid, Paper, Typography } from '@mui/material';
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 import { context } from '../../context';
 import testids from '../../helper/testids';
 
 const style = { display: 'flex', flexDirection: 'column', alignItems: 'center' };
 
-const OrderCard = ({ id, status, date, price }) => {
+const OrderCard = ({ id, status, saleDate, totalPrice }) => {
   const { user } = useContext(context);
+  const date = new Date(saleDate.toString());
+  const formatedDate = format(date, 'dd/MM/yy');
 
   return (
     <Grid
@@ -17,38 +20,36 @@ const OrderCard = ({ id, status, date, price }) => {
       sx={ { backgroundColor: '#e4e4e4' } }
       component={ Paper }
     >
-      <Grid item xs={ 1 } sx={ { ...style, backgroundColor: '#ffffff' } }>
-        <Typography variant="h6">Pedido</Typography>
+      <Grid item xs={ 2 } sx={ { ...style, backgroundColor: '#ffffff' } }>
+        <Typography>Pedido</Typography>
         <Typography
-          variant="h4"
           data-testid={ `${testids[user.role].orderCardID}${id}` }
         >
           { id }
         </Typography>
       </Grid>
       <Grid
+        fullWidth
         item
         xs={ 4 }
         component={ Paper }
         sx={ { ...style, backgroundColor: '#2FC18C' } }
         data-testid={ `${testids[user.role].orderCardStatus}${id}` }
       >
-        <Typography variant="h2">{ status.toUpperCase() }</Typography>
+        <Typography variant="h5">{ status.toUpperCase() }</Typography>
       </Grid>
-      <Grid item xs={ 2 }>
+      <Grid item xs={ 2 } fullWidth>
         <Typography
-          variant="h4"
           sx={ { ...style, backgroundColor: '#ffffff' } }
           data-testid={ `${testids[user.role].orderCardDate}${id}` }
         >
-          { date }
+          { formatedDate }
         </Typography>
         <Typography
-          variant="h4"
           sx={ { ...style, backgroundColor: '#ffffff' } }
           data-testid={ `${testids[user.role].orderCardPrice}${id}` }
         >
-          { `R$${price}` }
+          { `R$${totalPrice}` }
         </Typography>
       </Grid>
     </Grid>
@@ -58,8 +59,8 @@ const OrderCard = ({ id, status, date, price }) => {
 OrderCard.propTypes = {
   id: PropTypes.number.isRequired,
   status: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
+  saleDate: PropTypes.string.isRequired,
+  totalPrice: PropTypes.string.isRequired,
 };
 
 export default OrderCard;
