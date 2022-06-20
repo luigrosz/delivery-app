51e1a3adb12ea06d8648f66c800826e93c519030
Augusto Alves <85210880+GeZudo@users.noreply.github.com>
2022-06-20 18:30:10 -0300

    ✨ Feat: Delete user

--- front-end/src/pages/admin/UserTableRoll.jsx
+++ front-end/src/pages/admin/UserTableRoll.jsx
@@ -1,52 +1,70 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   Button,
   TableCell,
   TableRow,
 } from '@mui/material';
 import PropTypes from 'prop-types';
+import { context } from '../../context';
 
-const UserTableRoll = ({ index, name, email, role }) => (
-  <TableRow>
-    <TableCell
-      align="center"
-      data-testid={ `admin_manage__element-user-table-item-number-${index}` }
-    >
-      { index + 1 }
-    </TableCell>
-    <TableCell
-      align="center"
-      data-testid={ `admin_manage__element-user-table-name-${index}` }
-    >
-      { name }
-    </TableCell>
-    <TableCell
-      align="center"
-      data-testid={ `admin_manage__element-user-table-email-${index}` }
-    >
-      { email }
-    </TableCell>
-    <TableCell
-      align="center"
-      data-testid={ `admin_manage__element-user-table-role-${index}` }
-    >
-      { role }
-    </TableCell>
-    <TableCell
-      align="center"
-    >
-      <Button
-        type="submit"
-        variant="contained"
-        sx={ { height: '100%' } }
-        onClick={ () => { } }
-        data-testid={ `admin_manage__element-user-table-remove-${index}` }
+const UserTableRoll = ({ index, name, email, role }) => {
+  const { APIURL } = useContext(context);
+
+  const handleDelete = async () => {
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    console.log(email);
+    await fetch(`${APIURL}/user`, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: token,
+      },
+      body: JSON.stringify({ email }),
+    });
+  };
+
+  return (
+    <TableRow>
+      <TableCell
+        align="center"
+        data-testid={ `admin_manage__element-user-table-item-number-${index}` }
       >
-        excluir
-      </Button>
-    </TableCell>
-  </TableRow>
-);
+        { index + 1 }
+      </TableCell>
+      <TableCell
+        align="center"
+        data-testid={ `admin_manage__element-user-table-name-${index}` }
+      >
+        { name }
+      </TableCell>
+      <TableCell
+        align="center"
+        data-testid={ `admin_manage__element-user-table-email-${index}` }
+      >
+        { email }
+      </TableCell>
+      <TableCell
+        align="center"
+        data-testid={ `admin_manage__element-user-table-role-${index}` }
+      >
+        { role }
+      </TableCell>
+      <TableCell
+        align="center"
+      >
+        <Button
+          type="submit"
+          variant="contained"
+          sx={ { height: '100%' } }
+          onClick={ handleDelete }
+          data-testid={ `admin_manage__element-user-table-remove-${index}` }
+        >
+          excluir
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+};
 
 UserTableRoll.propTypes = {
   index: PropTypes.number.isRequired,
